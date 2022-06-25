2151a622b86451422088cd6641a02a550dbecc47
Dhionatan <dhionatan.dvg@gmail.com>
2022-06-24 21:19:19 -0300

    "Aprendendo mais do InteliJ"

---
+++ PrimeiroProgramaInteliJ/src/Main.java
@@ -0,0 +1,23 @@
+import br.com.dio.model.Gato;
+
+public class Main {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Jorge", "White", 20);
+
+        System.out.println("Oii Mundo!!");
+        System.out.println(gato);
+
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+        System.out.println("Oii Mundo!!");
+    }
+}
---
+++ PrimeiroProgramaInteliJ/src/br/com/dio/model/Gato.java
@@ -0,0 +1,65 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+
+    private String name;
+    private String color;
+    private Integer ager;
+
+    public Gato() {
+    }
+
+    public Gato(String name, String color, Integer ager) {
+        this.name = name;
+        this.color = color;
+        this.ager = ager;
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", ager=" + ager +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(name, gato.name) && Objects.equals(color, gato.color) && Objects.equals(ager, gato.ager);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color, ager);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getAger() {
+        return ager;
+    }
+
+    public void setAger(Integer ager) {
+        this.ager = ager;
+    }
+}
